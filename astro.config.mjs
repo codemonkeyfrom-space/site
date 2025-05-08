b19566e497ba9f2336b78665dc5ce0b7ba439cdc
Paul Abrams <john.paul.abrams@gmail.com>
2025-05-08 00:32:22 -0400

    fix: url config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://codemonkeyfromspace.com',
+	site: 'https://codemonkeyfrom.space',
 	base: '/',
 	integrations: [mdx(), sitemap()],
 	  
